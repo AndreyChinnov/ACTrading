8070f382a53dc1ff9b9320ed5c2d4ffff8580ef6
AndreyChinnov <andchinn@gmail.com>
2016-05-31 12:38:24 +0300

    Add new descriptions

---
+++ R/ResearchSupRes.R
@@ -0,0 +1,32 @@
+#Function that researches the Support and Resistant levels
+
+ResearchSupResFUN<-function(OHLC_Data, Delta) {
+  OHLC_Data$Bullish<- Cl(OHLC_Data)>=Op(OHLC_Data)
+  MaxVal<-max(Hi(OHLC_Data))+Delta
+  MinVal<-min(Lo(OHLC_Data))-Delta
+  p<-MinVal
+  Intervals<-NULL
+  DF<-NULL
+  while(p+Delta<MaxVal) {
+    Intervals<-c(Intervals,paste(as.character(p),as.character(p+Delta),sep="-"))
+    WhichInside<-Inside(p,Lo(OHLC_Data),Hi(OHLC_Data))|Inside(p+Delta,Lo(OHLC_Data),Hi(OHLC_Data))
+    N_cand<-sum(WhichInside)
+    #OHLC_Data[WhichInside]
+    N_cand_B<-sum(OHLC_Data[WhichInside]$Bullish)
+
+
+    # WhichReverse<-(Inside(Cl(OHLC_Data),p,p+Delta)&!Inside(Op(OHLC_Data),p,p+Delta))|
+    #   (Inside(Op(OHLC_Data),p,p+Delta)&!Inside(Cl(OHLC_Data),p,p+Delta))
+    WhichReverse<- !((Cl(OHLC_Data)>p+Delta & Op(OHLC_Data)<p) |
+                       (Cl(OHLC_Data)<p & Op(OHLC_Data)>p+Delta))& WhichInside
+
+    N_rev<-sum(WhichReverse)
+    N_rev_B<-sum(OHLC_Data[WhichReverse]$Bullish)
+    p_value<-(2*p+Delta)/2
+    newDF<-data.frame(p_value, N_cand, N_cand_B, N_rev, N_rev_B)
+    row.names(newDF)<-paste(as.character(p),as.character(p+Delta),sep="-")
+    DF<-rbind(DF,newDF)
+    p<-p+Delta
+  }
+  return(DF)
+}
