fe626f91dac1f00a2938b0d544497e0d79e4616e
AndreyChinnov <andchinn@gmail.com>
2016-06-03 19:02:19 +0300

    renamed Inside->NearBy

--- R/NearBy.R
+++ R/NearBy.R
@@ -1,4 +1,4 @@
-Inside<-function(p_start,p_end,low_values,high_values,error) {
+NearBy<-function(p_start,p_end,low_values,high_values,error) {
   return((p_start-high_values<=error & p_start-high_values>0)|
            (low_values-p_end<=error & low_values-p_end>0))
 }
